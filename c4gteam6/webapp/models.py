26bf95cafca84d11d3341be466ce015f7f6f8ae4
feliceeetan <felicetan3108@gmail.com>
2019-10-04 11:45:52 +0800

    database changes

--- c4gteam6/webapp/models.py
+++ c4gteam6/webapp/models.py
@@ -3,11 +3,18 @@ from django.db import models
 # Create your models here.
 
 class UserMetadata(models.Model):
-    submitTime = models.DateTimeField(auto_now_add=True, null=True)
-    date = models.DateField(null=True)
-
+    u_timestamp = models.DateTimeField(auto_now_add=True)
+    u_name = models.CharField(max_length=50)
+    u_age = models.IntegerField()
+    u_gender = models.CharFieldm(max_length=6)
+    u_race = models.CharField(max_length=20)
+    u_availdays = models.CharField(max_length=8)
+    u_availtime = models.CharField(max_length=10)
 
 class ReportData(models.Model):
-    submitTime = models.DateTimeField(auto_now_add=True, null=True)
-    date = models.DateField(null=True)
-
+    h_timestamp = models.DateTimeField(auto_now_add=True)
+    h_agerange = models.CharField(max_length=10)
+    h_gender = models.CharField(max_length=6)
+    h_race = modesl.CharField(max_length=20)
+    h_location = models.CharField(max_length=100)
+    h_description = models.TextField()
