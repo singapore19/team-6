e9aa5437eff81c18bc04d0d4c71beed61e7998dc
Jireh <jireh_tan@mymail.sutd.edu.sg>
2019-10-04 12:12:59 +0800

    updated heroku

--- c4gteam6/c4gteam6/settings.py
+++ c4gteam6/c4gteam6/settings.py
@@ -145,7 +145,6 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
